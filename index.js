d6eb273d8c3553bf7eb116b72962759e77c38a70
Isaac Hsu <16hsuisaac@gmail.com>
2021-11-29 00:20:22 -0800

    solved

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
 console.log('hello world')
 
-let datasetId = ''
+let final = {dealers: []}
 
-fetch('http://api.coxauto-interview.com/api/datasetId')
-  .then(response => response.json())
-  .then(data => datasetId = data);
+console.log(final);
 
-fetch(`http://api.coxauto-interview.com/${datasetId}/answer`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(datasetId),
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
+async function main() {
+  let datasetId = await fetch('http://api.coxauto-interview.com/api/datasetId')
+    .then(response => response.json())
+    .then(data => data.datasetId);
+
+  let vehicles = await fetch(`http://api.coxauto-interview.com/api/${datasetId}/vehicles`)
+    .then(response => response.json())
+    .then(data => data.vehicleIds);
+
+  let dealers = {}
+
+  for (let i = 0; i<vehicles.length; i++) {
+    let vehicleInfo = await fetch(`http://api.coxauto-interview.com/api/${datasetId}/vehicles/${vehicles[i]}`)
+      .then(response => response.json())
+      .then(data => data);
+    if (!dealers[vehicleInfo.dealerId]) {
+      let dealerInfo = await fetch(`http://api.coxauto-interview.com/api/${datasetId}/dealers/${vehicleInfo.dealerId}`)
+        .then(response => response.json())
+        .then(data => data);
+      let dealerId = vehicleInfo.dealerId
+      delete vehicleInfo.dealerId
+      dealers[dealerId] = dealerInfo
+      dealers[dealerId].vehicles = [vehicleInfo]
+    } else {
+      let dealerId = vehicleInfo.dealerId
+      delete vehicleInfo.dealerId
+      dealers[dealerId].vehicles.push(vehicleInfo);
+    }
+  }
+
+  for (x in dealers) {
+    final.dealers.push(dealers[x])
+  }
+
+
+  fetch(`http://api.coxauto-interview.com/api/${datasetId}/answer`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(final),
   })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        console.log('Success:', data);
+      } else {
+        console.error('Error:', data.message)
+      }
+      console.log(data.totalMilliseconds)
+
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+}
+
+main()
